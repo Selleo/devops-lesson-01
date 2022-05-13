a048da8a1e9f7a8dd63d4aa2e8a14721b0cbe4bc
qbart <wojtowicz.bartlomiej@gmail.com>
2022-05-13 12:18:09 +0200

    Move route53 resource to separate file

--- 04-frontend-apps/main.tf
+++ 04-frontend-apps/main.tf
@@ -16,30 +16,3 @@ module "app2" {
   aliases         = ["devpath.workshops.selleo.app"]
 }
 
-data "aws_route53_zone" "workshops" {
-  name = "workshops.selleo.app"
-}
-
-resource "aws_route53_record" "app1" {
-  zone_id = data.aws_route53_zone.workshops.zone_id
-  name    = "dashboard"
-  type    = "A"
-
-  alias {
-    name                   = module.app1.domain_name
-    zone_id                = module.app1.hosted_zone_id
-    evaluate_target_health = true
-  }
-}
-
-resource "aws_route53_record" "app2" {
-  zone_id = data.aws_route53_zone.workshops.zone_id
-  name    = "devpath"
-  type    = "A"
-
-  alias {
-    name                   = module.app2.domain_name
-    zone_id                = module.app2.hosted_zone_id
-    evaluate_target_health = true
-  }
-}
